af2fa4edfbe399d5ab7695e23b602f63e9a07f51
Madhur Agrawal <madhur.agrawal@impactqa.com>
2020-10-17 21:57:48 +0530

    ALert messages added to the functionality.

--- app.js
+++ app.js
@@ -43,6 +43,17 @@ class UI {
         }
     }
 
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        //Vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
     static clearFields(){
         document.getElementById('title').value='';
         document.getElementById('author').value='';
@@ -64,20 +75,31 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.getElementById('author').value;
     const isbn = document.getElementById('isbn').value;
 
-    //Instantiate book
-    const book = new Book(title, author, isbn);
+    //Validate
+    if(title===''||author===''||isbn==='') {
+        UI.showAlert('Please fill all the fields', 'danger');
+    }
+    else {
+        //Instantiate book
+        const book = new Book(title, author, isbn);
+    
+        //Add a book to UI
+        UI.addBookToList(book);
 
-    //  console.log(book);
-    //Add a book to UI
-    UI.addBookToList(book);
+        //Show success message
+        UI.showAlert('Book Added', 'success');
+    
+        //Clear the fields
+        UI.clearFields();
+    }
 
-    //Clear the fields
-    UI.clearFields();
 
 });
 
 //Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e)=> {
     UI.deleteBook(e.target);
-    //
-})
+    //Show success message
+    UI.showAlert('Book Removed', 'success'); 
+
+});
