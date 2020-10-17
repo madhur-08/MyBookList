9b9457fb5ebb2c8b1f3fe8af5821e831c47b6806
Madhur Agrawal <madhur.agrawal@impactqa.com>
2020-10-17 20:23:50 +0530

    demo commit

--- app.js
+++ app.js
@@ -79,4 +79,5 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 //Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e)=> {
     UI.deleteBook(e.target);
+    //
 })
